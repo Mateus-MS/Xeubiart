562e6e01b4045549cf73db211cbae58e8f60b277
mateusLWD <alves.mateus1050@gmail.com>
2025-11-19 15:24:16 +0000

    templ basic implementation

--- pkg/backend/routes/pages/ladingPage.go
+++ pkg/backend/routes/pages/ladingPage.go
@@ -2,12 +2,28 @@ package routes_pages
 
 import (
 	"net/http"
+	"strings"
 
+	desktop_page_home "github.com/Mateus-MS/Xeubiart.git/frontend/desktop/pages/home"
+	mobile_page_home "github.com/Mateus-MS/Xeubiart.git/frontend/mobile/pages/home"
 	"github.com/gin-gonic/gin"
 )
 
 func LandingPageRoute() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.String(http.StatusOK, "Welcome to the landing page")
+		c.Writer.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+		if strings.Contains(c.Request.UserAgent(), "mobile") {
+			err := mobile_page_home.Index().Render(c.Request.Context(), c.Writer)
+			if err != nil {
+				c.String(http.StatusInternalServerError, "render error: %v", err)
+			}
+			return
+		}
+
+		err := desktop_page_home.Index().Render(c.Request.Context(), c.Writer)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "render error: %v", err)
+		}
 	}
 }
