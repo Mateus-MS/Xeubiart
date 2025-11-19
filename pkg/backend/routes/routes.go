9b3585b0820c0fe66c623f220fc62f3d1ba31a01
mateusLWD <alves.mateus1050@gmail.com>
2025-11-19 15:57:20 +0000

    static file serving

--- pkg/backend/routes/routes.go
+++ pkg/backend/routes/routes.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/Mateus-MS/Xeubiart.git/backend/app"
 	routes_pages "github.com/Mateus-MS/Xeubiart.git/backend/routes/pages"
 	routes_utils "github.com/Mateus-MS/Xeubiart.git/backend/routes/utils"
@@ -12,4 +14,7 @@ func InitRoutes(app *app.App) {
 
 	// Utils
 	app.Router.GET("/health", routes_utils.HealtheRoute())
+
+	// Static
+	app.Router.StaticFS("/static", http.Dir("./frontend/static"))
 }
