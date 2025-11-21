0d3aa048d6682cf0e0c4fade1e1cb2ef2926e088
mateusLWD <alves.mateus1050@gmail.com>
2025-11-21 08:52:33 -0300

    mobile landing page hero section

--- pkg/frontend/mobile/pages/home/index_templ.go
+++ pkg/frontend/mobile/pages/home/index_templ.go
@@ -29,7 +29,7 @@ func Index() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Document</title><base href=\"static/mobile/home/\"><link rel=\"stylesheet\" href=\"css/main.css\" type=\"text/css\"><link rel=\"stylesheet\" href=\"css/header.css\" type=\"text/css\"></head><body><header><div id=\"logo\"></div><ul id=\"pages_links\"><li>Appointmasdasdasdsent</li><li>Contact</li></ul><div id=\"login-methods\"><button id=\"login\">Log In</button> <button id=\"register\">Create an account</button></div></header></body></html>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Document</title><base href=\"static/mobile/home/\"><link rel=\"stylesheet\" href=\"css/main.css\" type=\"text/css\"><link rel=\"stylesheet\" href=\"css/header.css\" type=\"text/css\"><link rel=\"stylesheet\" href=\"css/hero.css\" type=\"text/css\"></head><body><header><img id=\"logo\" src=\"../../svg/logo.svg\"><div id=\"login_methods\"><button id=\"login_button\">Log In</button> <button id=\"register_button\">Create an account</button></div></header><section id=\"hero_section\"><div id=\"CTA\"><h1>Delicadeza que marca</h1><p>Do traço leve ao marcante, cada tatuagem feita para <br>refletir quem você é.</p><div id=\"image\"></div><button>Agende já!</button></div></section></body></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
